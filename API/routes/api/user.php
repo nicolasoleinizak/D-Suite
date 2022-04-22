3c96a122ffa6d88808e6f5907602630afac57b90
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-04-22 10:03:29 -0300

    Add get permissions route

--- API/routes/api/user.php
+++ API/routes/api/user.php
@@ -8,6 +8,7 @@ Route::controller(UserController::class)->group(function(){
         'namespace' => 'App\Http\Controllers',
     ], function(){
         Route::post('/user', 'getUserParameters');
+        Route::post('/permissions/{organization_id}', 'getPermissions');
     });
     Route::post('/register', 'register');
 });

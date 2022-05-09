508549dd70b9955de0fc5e27be5263b7a8f67ac1
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-09 08:23:03 -0300

    Add image_url field

--- API/database/migrations/2022_04_13_160000_create_organizations_table.php
+++ API/database/migrations/2022_04_13_160000_create_organizations_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('image_url');
             $table->timestamps();
         });
     }

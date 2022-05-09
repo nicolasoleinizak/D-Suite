438b470693bf97be36b82d08bd232d104de4209a
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-09 08:25:05 -0300

    Add routes

--- API/routes/api/organization.php
+++ API/routes/api/organization.php
@@ -3,15 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizationController;
 
-Route::controller(ProductController::class)->group( function () {
+Route::controller(OrganizationController::class)->group( function () {
     Route::middleware(['api'])->group(function () {
+        Route::post('organizations/', 'create');
         Route::middleware('has.permission:1,1')->group( function () {
-            Route::get('organization/{id}', 'retrieve');
-            Route::delete('organizacion/{id}', 'destroy');
+            Route::get('organizations/{organization_id}', 'retrieve');
         });
         Route::middleware('has.permission:1,2')->group( function () {
-            Route::post('organization/', 'create');
-            Route::put('organization/{id}', 'update');
+            Route::put('organizations/{organization_id}', 'update');
+            Route::delete('organizations/{organization_id}', 'destroy');
+        });
+        Route::middleware('is.superadmin')->group( function () {
+            Route::get('organizations/', 'index');
         });
     });
 });

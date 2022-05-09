d4da85e2d470f781148175a1d55c2e2ab303cd11
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-09 08:24:14 -0300

    Add organization seeder

--- API/database/seeders/OrganizationSeeder.php
+++ API/database/seeders/OrganizationSeeder.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use App\Models\Organization;
+use App\Models\Module;
+use DateTime;
+use DateInterval;
 
 class OrganizationSeeder extends Seeder
 {
@@ -17,8 +21,19 @@ class OrganizationSeeder extends Seeder
     public function run()
     {
         DB::table('organizations')->delete();
-        DB::table('organizations')->insert([
-            'name' => 'Dulceres'
-        ]);
+        $organization = new Organization;
+        $organization->name = 'Dulceres';
+        $organization->image_url = '/ficticious_place.jpg';
+        $organization->save();
+        $modules = Module::all();
+        $licenses = $modules->map(function($module) {
+            $expires = new DateTime();
+            $expires->add(new DateInterval('P10Y'));
+            return [
+                'module_id' => $module->id,
+                'expires' => $expires
+            ];
+        });
+        $organization->updateLicenses($licenses);
     }
 }

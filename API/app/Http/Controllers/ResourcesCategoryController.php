feb85a6961e321e7a46bc6c8de850484ede1a525
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-09 18:30:09 -0300

    Create index function and declare others controllers

--- API/app/Http/Controllers/ResourcesCategoryController.php
+++ API/app/Http/Controllers/ResourcesCategoryController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ResourcesCategory;
+use App\Libraries\JSONResponse;
 
 class ResourcesCategoryController extends Controller
 {
-    //
+    /**
+     * It returns a JSON response of all the categories in the database
+     * 
+     * @param Request request The request object
+     * 
+     * @return A JSONResponse object.
+     */
+    public function index (Request $request) {
+
+        $categories = ResourcesCategory::where([
+            'organization_id' => $request->organization_id
+        ])->get();
+
+        return response()->json(new JSONResponse([], $categories));
+    }
+
+    public function retrieve (Request $request) {
+
+    }
+
+    public function create (Request $request) {
+
+    }
+
+    public function update (Request $request) {
+
+    }
+
+    public function destroy (Request $request) {
+
+    }
 }

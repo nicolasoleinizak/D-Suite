0a4a6de417da79dd370ea06765fdc4b0d3edc9bb
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-15 20:04:57 -0300

    feat: add uptade and destroy methods

--- API/app/Http/Controllers/ResourceController.php
+++ API/app/Http/Controllers/ResourceController.php
@@ -13,35 +13,35 @@ class ResourceController extends Controller
             $resources = Resource::where([
                 'organization_id' => $request->organization_id,
             ])->with('categories')->get();
-    
-            if($resources){
-                return Jasonres::success('', $resources);
-            } else {
-                return Jasonres::error('REQ002');
-            }
+            return Jasonres::sendData($resources);
         } else {
             return Jasonres::error('REQ001');
         }
     }
 
     public function retrieve(Request $request){
-        $resource = Resource::where([
-            'organization_id' => $request->organization_id,
-            'id' => $request->id
-        ])->with('categories')->first();
-
-        if($resource){
-            return Jasonres::success('', $resource);
-        } else {
-            return Jasonres::error('SRV001');
+        try{
+            $resource = Resource::where([
+                'organization_id' => $request->organization_id,
+                'id' => $request->id
+            ])->with('categories')->first();
+            return Jasonres::sendData($resource);
+        } catch(Exception $e){
+            return Jasronres::error('SRV001');
         }
     }
 
     public function create(Request $request){
         try{
+            $validationData = $request->validate([
+                'name' => 'required|string',
+                'price' => 'numeric',
+                'unit' => 'required|string',
+                'categories' => 'array'
+            ]);
             $resource = new Resource;
             $resource->name = $request->name;
-            $resource->price = $request->price;
+            $resource->price = isset($request->price)? $request->price : 0;
             $resource->unit = $request->unit;
             $resource->organization_id = $request->organization_id;
             if(isset($categories)){
@@ -55,11 +55,36 @@ class ResourceController extends Controller
     }
 
     public function update(Request $request){
-        $resource = Resource::where([
-            'organization_id' => $request->organization_id,
-            'id' => $request->id
-        ]);
+        try{
+            $resource = Resource::where([
+                'organization_id' => $request->organization_id,
+                'id' => $request->id
+            ])->first();
 
-        //COMPLETE...
+            $resource->name = isset($request->name)? $request->name : $resource->name;
+            $resource->price = isset($request->price)? $request->price : $resource->price;
+            $resource->unit = isset($request->unit)? $request->unit : $resource->unit;
+            if(isset($request->categories)){
+                $resource->assignCategories($request->categories);
+            }
+            $resource->save();
+
+            return Jasonres::success('Data successfully updated', $resource);
+
+        } catch(Exception $e){
+            return Jasonres::error('SRV001');
+        }
+    }
+
+    public function destroy(Request $request){
+        try{
+            Resource::where([
+                'organization_id' => $request->organization_id,
+                'id' => $request->id
+            ])->delete();
+            return Jasonres::success('Item successfully deleted');
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 }

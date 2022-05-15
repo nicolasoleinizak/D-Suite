c0f011baa8e7a0671a09981cdd0b1b6bd880df4e
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-15 19:01:06 -0300

    feat: add categories getter

--- API/app/Models/Resource.php
+++ API/app/Models/Resource.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Resource extends Model
 {
     use HasFactory;
+
+    public function assignCategories($categories){
+        foreach($categories as $categorie){
+            DB::table('resource_resources_category')->insert([
+                'resource_id' => $this->id,
+                'resources_category_id' => $category->id
+            ]);
+        }
+    }
+
+    public function categories(){
+        return $this->belongsToMany(ResourcesCategory::class);
+    }
 }

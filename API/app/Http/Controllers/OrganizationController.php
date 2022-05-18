ffde493198169223314c326d4bb15ee09540ad6f
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-18 13:18:19 -0300

    feat/use-jasonres-responses

--- API/app/Http/Controllers/OrganizationController.php
+++ API/app/Http/Controllers/OrganizationController.php
@@ -6,6 +6,7 @@ use App\Models\Organization;
 use App\Models\User;
 use App\Models\Module;
 use App\Models\License;
+use App\Libraries\Jasonres;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use DateTime;
@@ -19,12 +20,35 @@ class OrganizationController extends Controller
      */
     public function index()
     {
-        return Organization::all();
+        try{
+            $organizations = Organization::all();
+    
+            if($organizations){
+                return Jasonres::success('', $organizations);
+            } else {
+                return Jasonres::error('REQ002');
+            }
+
+        } catch (Exception $e) {
+            return Jasonres::error('SRV001');
+        }
     }
 
     public function retrieve(Request $request)
     {
-        return Organization::find($request->organization_id);
+        try{
+
+            $organization = Organization::find($request->organization_id);
+
+            if($organization){
+                return Jasonres::success('', $organization);
+            } else {
+                return Jasonres::error('REQ002');
+            }
+
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 
     /**
@@ -39,32 +63,56 @@ class OrganizationController extends Controller
             'image' => 'string'
         ]);
 
-        $user_id = Auth::guard()->user()->id;
-        $organization = new Organization;
-        $organization->name = $request->name;
-        $organization->image_url = $request->image_url;
-        $organization->save();
-        $organization_id = $organization->id;
+        try{
+
+            $user = Auth::guard()->user();
+
+            if(!$user){
+                return Jasonres::error('AUT001');
+            }
+
+            $user_id = Auth::guard()->user()->id;
+
+            $organization = new Organization;
+            $organization->name = $request->name;
+            $organization->image_url = $request->image_url;
+            if(!$organization->save()){
+                return Jasronres::error('SRV001');
+            }
+
+            $default_license_length = 60 * 24 * 60 * 60;
+            $date = new DateTime();
+            $default_license_expiration_date = $date->setTimestamp(time() + $default_license_length);
+            
+            $modules = Module::all();
+            if(!$modules){
+                return Jasonres::error('SRV001');
+            }
+    
+            foreach($modules as $module){
+                $license = new License;
+                $license->module_id = $module->id;
+                $license->organization_id = $organization->id;
+                $license->expires = $default_license_expiration_date;
+                if(!$license->save()){
+                    return Jasronres::error('SRV001');
+                }
+            }
+    
+            $user = User::find($user_id);
+            if(!$user){
+                Jasonres::error('SRV001');
+            }
+            $user->initializePermissions($organization->id, true);
+    
+            return Jasonres::success('Organization created successfully', $organization);
+            
+        } catch (Exception $e) {
+
+        } 
+
 
         
-        $default_license_length = 60 * 24 * 60 * 60;
-        $date = new DateTime();
-        $default_license_expiration_date = $date->setTimestamp(time() + $default_license_length);
-        
-        $modules = Module::all();
-
-        foreach($modules as $module){
-            $license = new License;
-            $license->module_id = $module->id;
-            $license->organization_id = $organization_id;
-            $license->expires = $default_license_expiration_date;
-            $license->save();
-        }
-
-        $user = User::find($user_id);
-        $user->initializePermissions($organization_id, true);
-
-        return response()->json(['message' => 'Organization created successfully']);
     }
 
     /**
@@ -75,11 +123,27 @@ class OrganizationController extends Controller
      */
     public function update(Request $request)
     {
-        $organization = Organization::find($request->organization_id);
-        $organization->name = isset($request->name) ? $request->name : $organization->name;
-        $organization->image_url = isset($request->image_url) ? $request->image_url : $organization->image_url;
-        $organization->save();
-        return response()->json(['message' => 'Data updated successfully']);
+        try{
+            $organization = Organization::where([
+                'id' => $request->organization_id
+                ])->first();
+    
+            if($organization){
+    
+                $organization->name = isset($request->name) ? $request->name : $organization->name;
+                $organization->image_url = isset($request->image_url) ? $request->image_url : $organization->image_url;
+    
+                if($organization->save()){
+                    return Jasonres::success('Organization successfully updated', $organization);
+                } else{
+                    return Jasonres::error('SRV001');
+                }
+            } else {
+                return Jasonres::error('REQ001');
+            }
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 
     /**
@@ -90,7 +154,22 @@ class OrganizationController extends Controller
      */
     public function destroy(Request $request)
     {
-        Organization::find($request->organization_id)->delete();
-        return response()->json(['message' => 'Organization deleted successfully']);
+        try{
+            $organization = Organization::where([
+                'id' => $request->organization_id
+            ]);
+
+            if($organization->first()){
+                if($organization->delete()){
+                    return Jasonres::success('The organization was successfully deleted');
+                } else {
+                    return Jasonres::error('SRV001');
+                }
+            } else {
+                return Jasonres::error('REQ002');
+            }
+        } catch (Exception $e) {
+            return Jasonres::error('SRV001');
+        }
     }
 }

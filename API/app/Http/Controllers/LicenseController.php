5fbaabf891ba972216540190a60073bb59e55dcd
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-18 12:03:41 -0300

    feat/use-jasonres-library

--- API/app/Http/Controllers/LicenseController.php
+++ API/app/Http/Controllers/LicenseController.php
@@ -4,16 +4,38 @@ namespace App\Http\Controllers;
 
 use App\Models\License;
 use Illuminate\Http\Request;
+use App\Libraries\Jasonres;
 
 class LicenseController extends Controller
 {
+    /**
+     * It updates the license expiration date
+     * 
+     * @param Request request 
+     * 
+     * @return A JSON response.
+     */
     public function update(Request $request){
-        $license = License::where([
-            'module_id' => $request->module_id,
-            'organization_id' => $request->organization_id
-        ])->get()->first();
-        $license->expires = $request->expires;
-        $license->save();
-        return response()->json(['message' => 'Licenses successfully updated']);
+        try{
+            $license = License::where([
+                'module_id' => $request->module_id,
+                'organization_id' => $request->organization_id
+            ])->first();
+
+            if(!$license){
+                return Jasonres::error('REQ002');
+            }
+
+            $license->expires = $request->expires;
+            
+            if($license->save()){
+                return Jasonres::success('License successfully updated');
+            } else {
+                return Jasonres::error('SRV001');
+            }
+
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 }

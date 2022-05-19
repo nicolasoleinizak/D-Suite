ac6bc8b0cf4d25677e0192409f814a27f044f605
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-18 21:32:51 -0300

    refactor/routes-and-controllers-names

--- API/routes/api/user.php
+++ API/routes/api/user.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\UserController;
 
 Route::controller(UserController::class)->group(function(){
+
     Route::middleware(['api']) ->group( function () {
-        Route::post('/user', 'getUserParameters');
+        Route::post('/me', 'getUserInfo');
         Route::get('me/permissions/{organization_id}', 'getPermissions');
     });
+
     Route::post('/register', 'register');
+
     Route::middleware(['has.permission:1,2'])->group(function(){
         Route::put('/permissions', 'updatePermissions');
     });
+    
 });

0e4bc62ca5578d292cc7b083fb01d76afe68303f
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-21 22:10:37 -0300

    feat: stock controller structure

---
+++ API/app/Http/Controllers/StockController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Libraries\Jasonres;
+use App\Models\Product;
+use App\Models\Resource;
+
+class StockController extends Controller
+{
+    public function index(Request $request){
+
+    }
+
+    public function retrieve(Request $request){
+        
+    }
+
+    public function update(Request $request){
+        
+    }
+}

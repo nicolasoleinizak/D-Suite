20ac3ebefb9214c870e1835478ca9720c1e64a0d
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-22 21:44:25 -0300

    feat: add controllers

--- API/app/Http/Controllers/StockController.php
+++ API/app/Http/Controllers/StockController.php
@@ -10,11 +10,68 @@ use App\Models\Resource;
 class StockController extends Controller
 {
     public function index(Request $request){
+        try {
 
+            $products = Product::where([
+                'organization_id' => $request->organization_id
+            ])->get();
+            $resources = Resource::where([
+                'organization_id' => $request->organization_id
+            ])->get();
+            if($products && $resources){
+                $items = [];
+                foreach($products as $product){
+                    array_push(
+                        $items, 
+                        [
+                            ...$product->toArray(),
+                            'type' => 'product'
+                    ]);
+                }
+                foreach($resources as $resource){
+                    array_push(
+                        $items,
+                        [
+                            ...$resource->toArray(),
+                            'type' => 'resource'
+                        ]
+                        );
+                }
+                return Jasonres::success('', $items);
+            } else {
+                return Jasonres::error('REQ002');
+            }
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 
     public function retrieve(Request $request){
-        
+        try {
+
+            $item = null;
+            switch($request->type){
+                case 'product':
+                    $item = Product::where([
+                        'organization_id' => $request->organization_id,
+                        'id' => $request->id
+                    ])->first();
+                    break;
+                case 'resource':
+                    $item = Resource::where([
+                        'organization_id' => $request->organization_id,
+                        'id' => $request->id
+                    ])->first();
+                    break;
+            }
+            if($item){
+                return Jasonres::success('', $item);
+            } else {
+                return Jasonres::error('REQ002');
+            }
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 
     public function update(Request $request){

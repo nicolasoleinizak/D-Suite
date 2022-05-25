965010d3fbd3efc2c372f5a943386c335ccaeef8
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-24 21:31:14 -0300

    feat: add udpate and updateAll controllers for stock

--- API/app/Http/Controllers/StockController.php
+++ API/app/Http/Controllers/StockController.php
@@ -75,6 +75,86 @@ class StockController extends Controller
     }
 
     public function update(Request $request){
-        
+        try {
+            echo $request->type;
+            switch($request->type){
+                case 'product':
+                    $product = Product::where([
+                        'organization_id' => $request->organization_id,
+                        'id' => $request->id
+                    ])->first();
+                    if($product){
+                        $product->stock = $request->stock;
+                        if($product->save()){
+                            return Jasonres::success('', $product);
+                        } else {
+                            return Jasonres::error('SRV001');
+                        }
+                    } else {
+                        return Jasonres::error('REQ002');
+                    }
+                    break;
+                case 'resource':
+                    $resource = Resource::where([
+                        'organization_id' => $request->organization_id,
+                        'id' => $request->id
+                    ])->first();
+                    if($resource){
+                        $resource->stock = $request->stock;
+                        if($resource->save()){
+                            return Jasonres::success('', $resource);
+                        } else {
+                            return Jasonres::error('SRV001');
+                        }
+                    } else {
+                        return Jasonres::error('REQ002');
+                    }
+                    break;
+            }
+        } catch (Exception $e) {
+            return Jasonres::error('SRV001');
+        }
+    }
+
+    public function updateAll (Request $request) {
+        try {
+            foreach($request->items as $item){
+                switch($item['type']){
+                    case 'product':
+                        $product = Product::where([
+                            'organization_id' => $request->organization_id,
+                            'id' => $item['id']
+                        ])->first();
+                        if($product){
+                            $product->stock = $item['stock'];
+                            if($product->save()){
+                            } else {
+                                return Jasonres::error('SRV001');
+                            }
+                        } else {
+                            return Jasonres::error('REQ002');
+                        }
+                        break;
+                    case 'resource':
+                        $resource = Resource::where([
+                            'organization_id' => $request->organization_id,
+                            'id' => $item['id']
+                        ])->first();
+                        if($resource){
+                            $resource->stock = $item['stock'];
+                            if($resource->save()){
+                            } else {
+                                return Jasonres::error('SRV001');
+                            }
+                        } else {
+                            return Jasonres::error('REQ002');
+                        }
+                        break;
+                }
+            }
+            return Jasonres::success('Items stock successfully updated');
+        } catch (Exception $e){
+            return Jasonres::error('SRV001');
+        }
     }
 }

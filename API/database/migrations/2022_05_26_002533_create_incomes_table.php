68502fc19f89125b8d35eb7e41326c3c03baafbc
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-25 21:43:15 -0300

    feat: add incomes and expenses migrations and models

---
+++ API/database/migrations/2022_05_26_002533_create_incomes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->float('quantity')->nullable();
+            $table->string('concept');
+            $table->double('value');
+            $table  ->foreignId('organization_id')
+                    ->constrained('organizations')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+};
---
+++ API/database/migrations/2022_05_26_002655_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('concept');
+            $table->double('value');
+            $table  ->foreignId('organization_id')
+                    ->constrained('organizations')
+                    ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+};

80e3c9337b5c625a157a6f3fd8f36fc082d81d1c
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-27 19:34:14 -0300

    fix: date types

--- API/database/seeders/IncomeSeeder.php
+++ API/database/seeders/IncomeSeeder.php
@@ -18,30 +18,27 @@ class IncomeSeeder extends Seeder
         DB::table('incomes')->insert([
             [
                 'type' => 'sale',
-                'date' => '2022-24-04',
+                'date' => date('2022-04-24 12:00:00'),
                 'quantity' => 1,
                 'concept' => 'Alfajores',
                 'value' => 150,
                 'organization_id' => 1,
-                'created_at' => '1990-12-12 12:00'
             ],
             [
                 'type' => 'sale',
-                'date' => '2022-10-04',
+                'date' => date('2022-10-04 12:00:00'),
                 'quantity' => 2,
                 'concept' => 'Alfajores',
                 'value' => 200,
                 'organization_id' => 1,
-                'created_at' => '1995-12-12 12:00'
             ],
             [
                 'type' => 'sale',
-                'date' => '2022-20-04',
+                'date' => date('2022-09-04 12:00:00'),
                 'quantity' => 2,
                 'concepto' => 'Bombones',
                 'value' => 200,
                 'organization_id' => 1,
-                'created_at' => '2000-12-12 12:00'
             ]
             ]);
     }

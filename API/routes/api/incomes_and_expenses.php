6d0fd9150b95819648512e3b545bed32c2f9409c
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-05-28 19:10:40 -0300

    feat: add income and expense routes

--- API/routes/api/incomes_and_expenses.php
+++ API/routes/api/incomes_and_expenses.php
@@ -6,9 +6,17 @@ use App\Http\Controllers\ExpenseController;
 
 Route::middleware(['api'])->group( function () {
     Route::controller(IncomeController::class)->group( function () {
-        Route::get('/{organization_id}/incomes/{after?}{before?}', 'index');
+        Route::get('/{organization_id}/incomes/{id}', 'retrieve');
+        Route::get('/{organization_id}/incomes/{after?}{before?}{page?}{limit?}', 'index');
+        Route::post('/{organization_id}/incomes/', 'create');
+        Route::put('/{organization_id}/incomes/{id}', 'update');
+        Route::delete('/{organization_id}/incomes/{id}', 'destroy');
     });
     Route::controller(ExpenseController::class)->group( function () {
-
+        Route::get('/{organization_id}/expenses/{id}', 'retrieve');
+        Route::get('/{organization_id}/expenses/{after?}{before?}{page?}{limit?}', 'index');
+        Route::post('/{organization_id}/expenses/', 'create');
+        Route::put('/{organization_id}/expenses/{id}', 'update');
+        Route::delete('/{organization_id}/expenses/{id}', 'destroy');
     });
 });

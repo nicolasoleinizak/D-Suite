a5490a14036b9464e586c969e7a352b6b5ccc168
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-06-03 19:43:24 -0300

    feat: add price calcul and updateMany controller

--- API/app/Http/Controllers/ResourceController.php
+++ API/app/Http/Controllers/ResourceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Libraries\Jasonres;
 use App\Models\Resource;
+use App\Libraries\PriceHandle\PricesChecker;
 
 class ResourceController extends Controller
 {
@@ -85,6 +86,16 @@ class ResourceController extends Controller
                     $resource->assignCategories($request->categories);
                 }
                 if($resource->save()){
+                    if(isset($request->price)){
+                        $modified_items = [
+                            [
+                                'class' => 'Resource',
+                                'id' => $resource->id
+                            ]
+                        ];
+                        $price_checker = new PricesChecker($modified_items, $request->organization_id);
+                        $price_checker->update();
+                    }
                     return Jasonres::success('Data successfully updated', $resource);
                 } else {
                     return Jasonres::error('SRV001');
@@ -94,6 +105,38 @@ class ResourceController extends Controller
             }
         } catch(Exception $e){
             return Jasonres::error('SRV001');
+        }
+    }
+
+    public function updateMany (Request $request) {
+        try{
+            $updated_items = [];
+            foreach($request->resources as $updated_resource){
+                $resource = Resource::where([
+                    'organization_id' => $request->organization_id,
+                    'id' => $updated_resource['id']
+                ])->first();
+                if($resource){
+                    $resource->name = isset($updated_resource['name'])? $updated_resource['name'] : $resource->name;
+                    $resource->unit = isset($updated_resource['unit'])? $updated_resource['unit'] : $resource->unit;
+                    $resource->stock = isset($updated_resource['stock'])? $updated_resource['stock'] : $resource->stock;
+                    $resource->price = isset($updated_resource['price'])? $updated_resource['price'] : $resource->price;
+                    if(!$resource->save()){
+                        return Jasonres::error('SRV001');
+                    }
+                } else {
+                    return Jasonres::error('REQ001');
+                }
+                array_push($updated_items, [
+                    'class' => 'Resource',
+                    'id' => $resource->id
+                ]);
+            }
+            $prices_checker = new PricesChecker($updated_items, $request->organization_id);
+            $prices_checker->update();
+            return Jasonres::success('Resources were successfully updated');
+        } catch (Exception $e) {
+
         }
     }
 

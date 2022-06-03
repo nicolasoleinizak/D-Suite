7912cd8438b928c78b95fe4d94e7a79b044ca9dd
Nicolás Oleinizak <nicolas.oleinizak@gmail.com>
2022-06-03 19:44:03 -0300

    feat: add prices routes

--- API/routes/api/prices.php
+++ API/routes/api/prices.php
@@ -2,10 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use App\Libraries\PriceCalculator;
+use App\Libraries\PriceHandle\PriceCalculator;
+use App\Http\Controllers\PriceMakerController;
 
 Route::middleware(['api'])->group( function () {
     Route::post('/{organization_id}/price_calculator/', function(Request $request){
         return PriceCalculator::parse($request->formula, $request->organization_id);
+    });
+    Route::controller(PriceMakerController::class)->group( function () {
+        Route::middleware(['has.permission:7,1'])->group( function () {
+            Route::get('/{organization_id}/price_maker/{id}', 'retrieve');
+        });
+        Route::middleware(['has.permission:7,2'])->group( function () {
+            Route::post('/{organization_id}/price_maker/', 'create');
+            Route::put('/{organization_id}/price_maker/{id}', 'update');
+        });
     });
 });
